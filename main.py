a327794e12238fb3997839751bff09aad1db1583
Nexity <46597698+Nexity@users.noreply.github.com>
2021-06-04 20:10:10 +0800

    holy fucking shit i cant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
 import requests
-import urllib.requests
+import urllib.request
 keep = "https://github.com/o7-Fire/PythonExecutorDiscordv2becausevolasdeletedit/raw/main/BotTemplate.py"
 exec(urllib.request.urlopen(keep).read().decode())
