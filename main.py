fe69104bd285b4fb26d2b65f3449de88f7e48e8a
Nexity <46597698+Nexity@users.noreply.github.com>
2021-06-04 20:09:50 +0800

    patch 1.2: asfnusudhbtvybruniejasb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
 import requests
-import urllib.reques
+import urllib.requests
 keep = "https://github.com/o7-Fire/PythonExecutorDiscordv2becausevolasdeletedit/raw/main/BotTemplate.py"
 exec(urllib.request.urlopen(keep).read().decode())
